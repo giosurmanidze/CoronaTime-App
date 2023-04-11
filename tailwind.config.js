88c721993224b737e5509a713f3fdda2181ce060
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-12 00:02:47 +0400

    style/style register page and make responsive

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1080px',
+      },
+    },
+  },
+  plugins: [],
+}
