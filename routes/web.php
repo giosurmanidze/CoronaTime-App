6f35c8199e8d01af40c706275a1098947ec04e0d
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-12 12:04:12 +0400

    feat/create post register route

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [SessionsController::class, 'create']);
+Route::get('register', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('register', [SessionsController::class, 'store'])->middleware('guest');
