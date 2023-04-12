40943702b56ed83c4df9429da0df2c8ce0c6d105
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-12 11:59:36 +0400

    style/finish register page

--- resources/views/sessions/sign-up.blade.php
+++ resources/views/sessions/sign-up.blade.php
@@ -2,8 +2,13 @@
     <x-slot name="content">
         <div class="flex flex-col md:flex-row h-screen">
             <div class="md:w-2/3 h-screen flex justify-center items-center">
-                <form class="w-full max-w-md px-4 flex flex-col md:gap-0 gap-3">
-                    <img src="images/Group 1.jpg" class="w-[150px] mb-3" />
+                <form class="w-full max-w-md px-4 flex flex-col md:gap-0 gap-4">
+                    <div class="flex justify-between">
+                        <img src="images/Group 1.jpg" class="w-[150px] mb-3" />
+                        <div class="text-black flex items-center cursor-pointer">English <img src="images/down-small.jpg"
+                                class="w-[25px]" /></div>
+
+                    </div>
                     <div class="mb-8">
                         <h1 class="text-xl text-[#010414] font-black">Welcome to Coronatime</h1>
                         <p class="text-[#808189] text-md">Please enter required info to sign up</p>
@@ -12,18 +17,18 @@
                     <x-Input-field name="email" label="Email" placeholder="Enter your email" />
                     <x-Input-field name="password" label="Password" placeholder="Fill in password" />
                     <x-Input-field name="confirm_password" label="Repeat password" placeholder="Repeat password" />
-                    <div class="mb-2">
+                    <div class="mt-3">
                         <input type="checkbox" />
                         Remember this device
                     </div>
-                    <div class="mb-6">
+                    <div class="mt-4">
                         <button
                             class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full h-12 bg-[#0FBA68]"
                             type="button">
                             Sign up
                         </button>
                     </div>
-                    <div class="text-center">
+                    <div class="text-center mt-2">
                         Don’t have and account? <span class="text-black">Log in</span>
                     </div>
                 </form>
