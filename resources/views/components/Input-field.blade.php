d95d4005ce99d65a525ad987b4356774c2faa6bc
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-12 11:59:36 +0400

    style/finish register page

--- resources/views/components/Input-field.blade.php
+++ resources/views/components/Input-field.blade.php
@@ -1,10 +1,10 @@
-@props(['name', 'placeholder', "label"])
+@props(['name', 'placeholder', 'label'])
 
-<div class="mb-4">
+<div class="2xl:mt-3">
     <label class="block text-[#010414] font-bold mb-2" for="{{ $name }}">
         {{ $label }}
     </label>
     <input
-    class="appearance-none border h-14 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" }}
-        id="{{ $name }}" type="text" placeholder="{{ $placeholder }}">
-</div>
+        class="appearance-none border h-14 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        }} id="{{ $name }}" type="text" placeholder="{{ $placeholder }}">
+    </div>
