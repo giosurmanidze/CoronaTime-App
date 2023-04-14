bf0822eb29937165532ca659557655b93867b4f5
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-14 21:29:07 +0400

    refactor/routes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('register', 'sessions.sign-up')->middleware('guest');
-Route::post('register', [RegisterController::class, 'register'])->name('register');
+
+Route::middleware('guest')->group(function () {
+    Route::view('register', 'sessions.sign-up');
+    Route::post('register', [RegisterController::class, 'register'])->name('register');
+    Route::view('login', 'sessions.login');
+});
 
 Route::get('confirm-account/{user}', [RegisterController::class, 'confirmEmail'])->name('confirm-account');
-
-Route::view('login', 'sessions.login')->middleware('guest');
-
-
 Route::view('confirmation-status', 'email.confirmation-message');
