2f3a3639ccc591f77ffbc720eb99286068a0de72
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-15 22:30:59 +0400

    fix/styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        '2xl': '1080px',
-      },
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            screens: {},
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
