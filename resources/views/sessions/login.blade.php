f47a8d5081a3073d44e057fc7855f9b426e47a2b
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-15 23:16:48 +0400

    style/login page

--- resources/views/sessions/login.blade.php
+++ resources/views/sessions/login.blade.php
@@ -1,1 +1,54 @@
-login 
+<x-layout>
+    <x-slot name="content">
+        <div class="flex flex-col md:flex-row h-screen">
+            <div class="md:w-3/5 h-screen flex justify-center md:justify-start lg:ml-10  items-center w-full">
+                <form method="POST" action="/register" class="w-full max-w-md px-4 md:ml-10 flex flex-col md:gap-2 gap-4">
+                    @csrf
+
+                    <div class="flex justify-between">
+                        <img src="images/Group 1.jpg" class="w-[150px] mb-3" />
+                        <div class="text-black flex items-center cursor-pointer">
+                            <select id="language-select" onchange="window.location.href = this.value;">
+                                <option value="{{ route('login', ['language' => 'en']) }}"
+                                    @if (app()->getLocale() == 'en') selected @endif>English</option>
+                                <option value="{{ route('login', ['language' => 'ka']) }}"
+                                    @if (app()->getLocale() == 'ka') selected @endif>Georgian</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="mb-8">
+                        <h1 class="text-xl text-[#010414] font-black">{{ __('title_login') }}</h1>
+                        <p class="text-[#808189] text-md mt-1">{{ __('info_login') }}</p>
+                    </div>
+                    <x-Input-field type="text" name="name" label="user_name" placeholder="for_user"
+                        :errors="$errors" />
+                  
+                    <x-Input-field type="password" name="password_confirmation" label="conf_password"
+                        placeholder="for_conf_password" :errors="$errors" />
+                    <div class="mt-3 flex justify-between">
+                        <div class="flex gap-2">
+                            <input class="form-check-input w-[15px]" type="checkbox" name="remember_device" id="remember_device">
+                           <p class="text-sm text-[#010414] font-bold">{{ __('remember') }}</p> 
+                        </div>
+                        <a class="text-sm w-full text-[#2029F3] font-bold flex justify-end" href="#">{{__("forgot_password") }}</a>
+                    </div>
+                    <div class="mt-4">
+                        <button name="register"
+                            class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full h-12 bg-[#0FBA68]"
+                            type="submit">
+                            {{ __('login') }}
+                        </button>
+                    </div>
+                    <div class="text-center mt-2">
+                        {{ __('dont_have') }} <strong><a href="/login"
+                                class="text-black">{{ __('login_sign_up') }}</a></strong>
+                    </div>
+                </form>
+            </div>
+            <div class="md:w-2/5 bg-gray-300 bg-cover bg-center flex-shrink-0"
+                style="background-image: url('images/Rectangle 1.jpg');">
+            </div>
+        </div>
+    </x-slot>
+</x-layout>
