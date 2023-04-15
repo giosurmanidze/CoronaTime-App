cf5c630ddcc084508b8206e9ffbe921c032712ed
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-15 23:16:48 +0400

    style/login page

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::view('register', 'sessions.sign-up');
     Route::post('register', [RegisterController::class, 'register'])->name('register');
-    Route::view('login', 'sessions.login');
+    Route::view('login', 'sessions.login')->name("login");
+    Route::get('confirm-account/{user}', [RegisterController::class, 'confirmEmail'])->name('confirm-account');
 });
 
-Route::get('confirm-account/{user}', [RegisterController::class, 'confirmEmail'])->name('confirm-account');
 Route::view('confirmation-status', 'email.confirmation-message');
