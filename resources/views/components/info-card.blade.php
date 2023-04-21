1d6778714bb4424ae3225840fca23b8da0868191
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-21 22:52:42 +0400

    fix/issues

---
+++ resources/views/components/info-card.blade.php
@@ -0,0 +1,8 @@
+@props(['class','img', 'bg_color',"title"])
+
+<div class="h-[255px] rounded-2xl relative {{ $class }} flex flex-col items-center justify-center gap-3">
+    <img src="/images/{{ $img }}"/>
+    <h3 class="text-[#010414] text-base">{{ __($title) }}</h3>
+   <strong class="text-3xl">549,545</strong>
+    <div class="absolute inset-0 {{ $bg_color }} opacity-[.08] rounded-2xl"></div>
+</div>
