e0de1c4b4192672ceebb5b87fb0791413c6b4b54
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-22 22:32:42 +0400

    fix/add types

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Statistics;
+use Illuminate\View\View;
 
 class dashboardController extends Controller
 {
-    public function getWorldwideStatistics()
+    public function getWorldwideStatistics(): View
     {
         $worldwideStatistics = Statistics::where('code', 'Ww')->first();
         return view('components.landing-worldwide', ['worldwideStatistics' => $worldwideStatistics]);
