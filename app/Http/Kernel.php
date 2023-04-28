939a0dd1d334068204b2527003547fc0a3cd236b
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-29 01:19:33 +0400

    fix/redirect to home

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'redirect.if.root' => \App\Http\Middleware\RedirectIfRoot::class,
     ];
 }
